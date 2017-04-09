5720e7e4c3c9ef47212ba50dce543574ebe57dfc
Ruby <526852554@qq.com>
2017-04-09 16:04:03 +0800

    commit order/pages

--- js/order.js
+++ js/order.js
@@ -1,9 +1,16 @@
-window.onload=function(){
-    var ajax = new XMLHttpRequest();
-    ajax.open("GET", "/font/icons/svg-symbols.svg", true);
-    ajax.onload = function(e) {
-        document.body.insertAdjacentHTML("afterBegin", '<div style="display:none;">' + ajax.responseText + '</div>');
-    }
-    ajax.send();
-}
+// 订单结算
+$(".weui-cell__bd .weui-btn").on('click', function() {
+    $(this).addClass('weui-btn_blue');
+    $(this).siblings().addClass('weui-btn_default');
+    $(this).siblings().removeClass('weui-btn_blue');
+});
 
+// 选择取货时间
+$("#time").datetimePicker({
+        title: '出发时间',
+        min: "1990-12-12",
+        max: "2022-12-12 12:12",
+        onChange: function (picker, values, displayValues) {
+          console.log(values);
+        }
+      })
